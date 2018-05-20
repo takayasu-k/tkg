b512e81ef9cf4657fcbef673cc85615145ea25f0
takayasu-k <takayasu7468@gmail.com>
2018-05-20 16:22:51 +0900

    20180520 木曽 Moreタブの画面を追加

--- TKG/ViewController.swift
+++ TKG/ViewController.swift
@@ -10,16 +10,41 @@
 // Moreタブ用ビューコントローラ（暫定）
 import UIKit
 
-class ViewController: UIViewController {
+let settingsTitle = ["最新情報", "フィードバック", "アプリについて", "応援する"]
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  @IBOutlet weak var settingsTableView: UITableView!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    settingsTableView.delegate = self
+    settingsTableView.dataSource = self
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    // テーブルビュー必須メソッド：セクションの数
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // テーブルビュー必須メソッド：テーブルの行数
+    return settingsTitle.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//     テーブルビュー必須メソッド：セルの設定
+    let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+    let settingsData = settingsTitle[(indexPath as NSIndexPath).row]
+    cell.textLabel?.text = settingsData
+
+    return cell
   }
 
 
